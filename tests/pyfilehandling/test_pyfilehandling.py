beff7de10359935402c509f15aa23f03c05902a9
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-10-16 18:03:46 +0530

    add some tests

---
+++ tests/pyfilehandling/test_pyfilehandling.py
@@ -0,0 +1,10 @@
+import pyfilehandling
+
+def test_create_is_not_none():
+    assert pyfilehandling.create != None
+
+def test_read_is_not_none():
+    assert pyfilehandling.read != None
+
+def test_write_is_not_none():
+    assert pyfilehandling.write != None
