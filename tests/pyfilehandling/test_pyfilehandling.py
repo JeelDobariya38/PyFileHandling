67c73ba27e74506d8a16a0526c98078f39ce3e3d
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-10-17 12:17:26 +0530

    test the presence at package level

--- tests/pyfilehandling/test_pyfilehandling.py
+++ tests/pyfilehandling/test_pyfilehandling.py
@@ -1,10 +1,7 @@
 import pyfilehandling
 
-def test_create_is_not_none():
-    assert pyfilehandling.create != None
-
-def test_read_is_not_none():
+def test_read_is_present_at_package_level():
     assert pyfilehandling.read != None
 
-def test_write_is_not_none():
+def test_write_is_present_at_package_level():
     assert pyfilehandling.write != None
