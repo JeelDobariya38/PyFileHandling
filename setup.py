5eae48b7124f925724d714a46b9a0579c3818009
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-10-21 16:25:47 +0530

    improved setup.py

--- setup.py
+++ setup.py
@@ -12,19 +12,24 @@ setup(
     description='A Python package for file manipulation operations.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='JeelDobariya',
+    author='Jeel Dobariya',
     author_email='dobariyaj34@gmail.com',
     url='https://github.com/JeelDobariya38/PyFileHandling',
     packages=['pyfilehandling'],
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    keywords=['file handling','file manipulation','file operation'],
+    keywords=['file handling', 'file manipulation', 'file operation', 'utility'],
     project_urls={
         'Bug Tracker': 'https://github.com/JeelDobariya38/PyFileHandling/issues',
         'Documentation': 'https://jeeldobariya38.github.io/PyFileHandling/',
@@ -35,4 +40,13 @@ setup(
     },
     license='MIT',
     python_requires='>=3.9',
+    install_requires=[
+        # Add any required dependencies here
+    ],
+    extras_require={
+        'testing': [
+            'pytest',
+            'coverage',
+        ],
+    },
 )
