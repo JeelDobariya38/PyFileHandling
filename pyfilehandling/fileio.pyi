c1017730e70e65c9490c3e619dc54bb32274fa6d
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-10-21 14:19:36 +0530

    added some new set function to pyfilehandling
    - create_dir(path)
    - create_dir(path)
    - create_file(path)
    - remove_file(path)
    - writeline(path, data, mode="a")
    - writelines(path, data_list, mode="a")
    - readline(path)
    - readlines(path)
    - get_reader(path)

--- pyfilehandling/fileio.pyi
+++ pyfilehandling/fileio.pyi
@@ -1,6 +1,62 @@
-from typing import Optional
+from typing import Optional, List, Generator
 
-def write(path: str, data: str, mode: Optional[str]) -> None:
+def create_dir(path: str) -> None:
+    """
+    Create a directory at the given path.
+
+    Parameters:
+    - path (str): The path for the directory.
+
+    Raises:
+    - ValueError: If the path is invalid or a directory already exists at the given path.
+
+    Returns:
+    - None
+    """
+
+def remove_dir(path: str) -> None:
+    """
+    Remove a directory at the given path.
+
+    Parameters:
+    - path (str): The path to the directory to be removed.
+
+    Raises:
+    - ValueError: If the path is invalid.
+
+    Returns:
+    - None
+    """
+
+def create_file(path: str) -> None:
+    """
+    Create a file at the given path.
+
+    Parameters:
+    - path (str): The path for the file.
+
+    Raises:
+    - ValueError: If the path is invalid or a file already exists at the given path.
+
+    Returns:
+    - None
+    """
+
+def remove_file(path: str) -> None:
+    """
+    Remove a file at the given path.
+
+    Parameters:
+    - path (str): The path to the file to be removed.
+
+    Raises:
+    - ValueError: If the path is invalid.
+
+    Returns:
+    - None
+    """
+
+def write(path: str, data: str, mode: Optional[str] = 'a') -> None:
     """
     Write data to a file.
 
@@ -17,7 +73,41 @@ def write(path: str, data: str, mode: Optional[str]) -> None:
     - None
     """
 
-def read(path: str) -> None:
+def writeline(path: str, data: str, mode: Optional[str] = 'a') -> None:
+    """
+    Write data to a file on a new line.
+
+    Parameters:
+    - path (str): The path to the file.
+    - data (str): The data to be written to the file.
+    - mode (str, optional): The mode in which the file is opened. 
+      Defaults to 'a' (append). Other valid modes are 'w' (write).
+
+    Raises:
+    - ValueError: If an invalid mode is provided.
+
+    Returns:
+    - None
+    """
+
+def writelines(path: str, data_list: List[str], mode: Optional[str] = 'a') -> None:
+    """
+    Write a list of data to a file with each item on a new line.
+
+    Parameters:
+    - path (str): The path to the file.
+    - data_list (List[str]): The list of data to be written to the file.
+    - mode (str, optional): The mode in which the file is opened. 
+      Defaults to 'a' (append). Other valid modes are 'w' (write).
+
+    Raises:
+    - ValueError: If an invalid mode is provided.
+
+    Returns:
+    - None
+    """
+
+def read(path: str) -> str:
     """
     Read data from a file.
 
@@ -28,4 +118,43 @@ def read(path: str) -> None:
     - str: The content of the file.
 
     If the file is not found, an empty string is returned.
+    """
+
+def readline(path: str) -> List[str]:
+    """
+    Read lines from a file.
+
+    Parameters:
+    - path (str): The path to the file.
+
+    Returns:
+    - List[str]: A list of lines from the file with newline characters removed.
+
+    If the file is not found, an empty list is returned.
+    """
+
+def readlines(path: str) -> List[str]:
+    """
+    Read lines from a file.
+
+    Parameters:
+    - path (str): The path to the file.
+
+    Returns:
+    - List[str]: A list of lines from the file with newline characters removed.
+
+    If the file is not found, an empty list is returned.
+    """
+
+def get_reader(path: str) -> Generator[str, None, None]:
+    """
+    Return a generator for reading lines from a file.
+
+    Parameters:
+    - path (str): The path to the file.
+
+    Yields:
+    - str: Lines from the file with newline characters removed.
+
+    If the file is not found, the generator is empty.
     """
