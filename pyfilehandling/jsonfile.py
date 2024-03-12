abbaadaf8f78a89ce200779c5e5c45b3a2f1f3e6
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-03-12 20:53:05 +0530

    add two method for read and write

--- pyfilehandling/jsonfile.py
+++ pyfilehandling/jsonfile.py
@@ -1,8 +1,18 @@
 import json
+from typing import Dict
+
 
 class jsonFile():
-    def __init__(self, filename: str, mode: str):
+    def __init__(self, filename: str, mode: str = "r"):
         if not filename.endswith(".json"):
             filename += ".json"
         self.file = open(filename, mode)
 
+    def read(self) -> Dict:
+        if self.file.readable:
+            return json.load(self.file)
+        raise Exception("")
+
+    def write(self, data: Dict, indent: int = 4):
+        if self.file.writable:
+            json.dump(data, self.file, indent)
