c80b9c75da0f511be57c81efd53ec6ae6b666992
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-03-12 21:29:25 +0530

    resolve linting issues

--- pyfilehandling/jsonfile.py
+++ pyfilehandling/jsonfile.py
@@ -3,6 +3,7 @@ from .customerrors import ReadNotPermitted, WriteNotPermitted
 import json
 from typing import Dict, Any
 
+
 class jsonFile:
     def __init__(self, filename: str, mode: str = "r"):
         if not filename.endswith(".json"):
@@ -25,8 +26,8 @@ class jsonFile:
             json.dump(data, self.file, indent)
         raise WriteNotPermitted(self.file.name)
 
-    def read_key(self, key) -> Any:
-        if self.data == None:
+    def read_key(self, key: str) -> Any:
+        if self.data is None:
             self.data = self.read()
         else:
             return self.data[key]
