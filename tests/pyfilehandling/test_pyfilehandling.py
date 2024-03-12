1c39fa1fea9a03d90c00f101c8f986882f1dc990
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-03-12 21:52:43 +0530

    removed tests

--- tests/pyfilehandling/test_pyfilehandling.py
+++
@@ -1,7 +0,0 @@
-import pyfilehandling
-
-def test_read_is_present_at_package_level():
-    assert pyfilehandling.read != None
-
-def test_write_is_present_at_package_level():
-    assert pyfilehandling.write != None
