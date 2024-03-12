41721c83739cbabdf8fc5bdbb8e4743ce1e4070f
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-03-12 21:01:42 +0530

    add custom errors

--- pyfilehandling/jsonfile.py
+++ pyfilehandling/jsonfile.py
@@ -1,8 +1,10 @@
+from .customerrors import ReadNotPermitted, WriteNotPermitted
+
 import json
 from typing import Dict
 
 
-class jsonFile():
+class jsonFile:
     def __init__(self, filename: str, mode: str = "r"):
         if not filename.endswith(".json"):
             filename += ".json"
@@ -11,8 +13,9 @@ class jsonFile():
     def read(self) -> Dict:
         if self.file.readable:
             return json.load(self.file)
-        raise Exception("")
+        raise ReadNotPermitted(self.file.name)
 
     def write(self, data: Dict, indent: int = 4):
         if self.file.writable:
             json.dump(data, self.file, indent)
+        raise WriteNotPermitted(self.file.name)
